6d96874194e278c3684a62019ad8e3223d301c9a
Tensor <narutokelechi@gmail.com>
2024-07-20 10:55:04 +0100

    add: resnet image encoder for feature extraction

--- clip_model/clip_model.py
+++ clip_model/clip_model.py
@@ -1,2 +1,24 @@
 import torch
+import timm
 from torch import nn
+from torchvision import models
+from transformers import AutoTokenizer, AutoModelForTextEncoding
+from config import config
+
+
+tokenizer = AutoTokenizer.from_pretrained(config.text_encoder_id)
+
+
+class ImageEncoder(nn.Module):
+    def __init__(self, config: config, pretrained=True):
+        super().__init__()
+        self.resnet = timm.create_model(
+            model_name="resnet50", pretrained=True, num_classes=0, pool_layer="avg"
+        )
+
+        for par in self.resnet.parameters():
+            par.requires_grad = True
+
+    def forward(self, image):
+        x = self.resnet(image)
+        return x
